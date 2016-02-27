552cafca0611572e17fd296f6cac664d6d307221
nicolas.falmeida1 <nicolas.falmeida1@CAS419196W0729.senacsp.edu.br>
2016-02-26 21:03:36 -0300

    primeiro commit

---
+++ agenda/src/main/java/br/senac/tads/pi3/duplasubstitutadetonystark/agenda/Agenda.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.pi3.duplasubstitutadetonystark.agenda;
+
+/**
+ *
+ * @author nicolas.falmeida1
+ */
+public class Agenda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("teste");
+    }
+    
+}
