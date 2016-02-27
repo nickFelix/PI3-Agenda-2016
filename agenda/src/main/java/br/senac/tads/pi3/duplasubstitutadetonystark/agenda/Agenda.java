9c170673f39d82bec28e770e6d4c6493028d7395
giovane.gpecora1 <giovane.gpecora1@CAS411429W0730.senacsp.edu.br>
2016-02-26 22:28:39 -0300

    Conectando ao banco

--- agenda/src/main/java/br/senac/tads/pi3/duplasubstitutadetonystark/agenda/Agenda.java
+++ agenda/src/main/java/br/senac/tads/pi3/duplasubstitutadetonystark/agenda/Agenda.java
@@ -5,11 +5,25 @@
  */
 package br.senac.tads.pi3.duplasubstitutadetonystark.agenda;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author giovane.gpecora1
  */
 public class Agenda {
-    
+    private Connection obterConexao() throws SQLException, ClassNotFoundException{
+        Connection conn= null;
+        //Passo 1: Registrar driver JDBC.
+        Class.forName("org.apache.derby.jdbc.ClientDataSource");
+        
+        //Passo 2: Abrir a conexão
+        conn = DriverManager.getConnection("jdbc:derby://localhost:1527//agendabd;SecurityMechanism=3",
+        "app",//usuario
+        "app");//senha
+        return conn;
+    }
     
 }
