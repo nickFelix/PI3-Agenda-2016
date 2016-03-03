91fc40c3d9c1a44ddf674e6bf26633db631d4ff8
Giovane Pecora <Giovane Pecora@192.168.0.145>
2016-03-03 10:06:48 -0300

    Apaguei a classe testaagenda e coloquei o código do professor

--- agenda/src/main/java/br/senac/tads/pi3/duplasubstitutadetonystark/agenda/Agenda.java
+++ agenda/src/main/java/br/senac/tads/pi3/duplasubstitutadetonystark/agenda/Agenda.java
@@ -6,24 +6,77 @@
 package br.senac.tads.pi3.duplasubstitutadetonystark.agenda;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
- * @author giovane.gpecora1
+ * @author Giovane Pecora e Nicolas
  */
 public class Agenda {
-    private Connection obterConexao() throws SQLException, ClassNotFoundException{
-        Connection conn= null;
-        //Passo 1: Registrar driver JDBC.
+
+    private Connection obterConexao() throws SQLException, ClassNotFoundException {
+        Connection conn = null;
+        // Passo 1: Registrar driver JDBC.
         Class.forName("org.apache.derby.jdbc.ClientDataSource");
-        
-        //Passo 2: Abrir a conexão
-        conn = DriverManager.getConnection("jdbc:derby://localhost:1527//agendabd;SecurityMechanism=3",
-        "app",//usuario
-        "app");//senha
+
+        // Passo 2: Abrir a conexÃ£o
+        conn = DriverManager.getConnection(
+                "jdbc:derby://localhost:1527/agendabd;SecurityMechanism=3",
+                "app", // usuario
+                "app"); // senha
         return conn;
     }
-    
+
+    public void listarPessoas() {
+        Statement stmt = null;
+        Connection conn = null;
+
+        String sql = "SELECT ID_PESSOA, NM_PESSOA, DT_NASCIMENTO, VL_TELEFONE, VL_EMAIL FROM TB_PESSOA";
+        try {
+            conn = obterConexao();
+            stmt = conn.createStatement();
+            ResultSet resultados = stmt.executeQuery(sql);
+
+            DateFormat formatadorData = new SimpleDateFormat("dd/MM/yyyy");
+
+            while (resultados.next()) {
+                Long id = resultados.getLong("ID_PESSOA");
+                String nome = resultados.getString("NM_PESSOA");
+                Date dataNasc = resultados.getDate("DT_NASCIMENTO");
+                String email = resultados.getString("VL_EMAIL");
+                String telefone = resultados.getString("VL_TELEFONE");
+                System.out.println(String.valueOf(id) + ", " + nome + ", " + formatadorData.format(dataNasc) + ", " + email + ", " + telefone);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(Agenda.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Agenda.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(Agenda.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(Agenda.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+
 }
+
